ec77ec68a6019a100f1e09ad5f0c5afa21825b4d
Yuvadi29 <adityatrivedi612@gmail.com>
2025-11-29 22:56:43 +0530

    Running an agent using an API

--- agent_with_tools.js
+++ agent_with_tools.js
@@ -1,30 +1,40 @@
-import { Agent, run, tool } from "@openai/agents";
-import z from "zod";
+import { Agent, run, tool } from '@openai/agents'
+import z from 'zod'
+import axios from 'axios';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 // Creating a tool to get weather of a given city
 const getWeather = tool({
-    name: 'get_weathter',
-    description: 'Return the weather of a given city.',
-    parameters: z.object({
-        city: z.string()
-    }),
-    // Here we are just hardcoding the response
-    async execute({city}){
-        return `The weather in ${city} is sunny.`;
-    },
+  name: 'get_weathter',
+  description: 'Return the weather of a given city.',
+  parameters: z.object({
+    city: z.string().describe('Name of the city'),
+  }),
+  // Here we are just hardcoding the response
+  // async execute({city}){
+  //     return `The weather in ${city} is sunny.`;
+  // },
 
+  // Let's make an api call to get realtime weather data
+  async execute ({ city }) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&appid=${process.env.WEATHER_API_KEY}`
+    const response = await axios.get(url, {responseType: 'text'})
+    return `Weather of ${city} is ${response?.data}`;;
+  }
 });
 
 const agent = new Agent({
-    name: 'Weather Bot',
-    instructions: 'You are a helpful weather bot',
-    model: 'gpt-4.1',
-    tools:[getWeather],
-});
+  name: 'Weather Bot',
+  instructions: 'You are a helpful weather bot',
+  model: 'gpt-4.1',
+  tools: [getWeather]
+})
 
-async function runAgentWithTool() {
-    const result = await run(agent,'Bengaluru');
-    console.log(result?.finalOutput);
+async function runAgentWithTool () {
+  const result = await run(agent, 'What is the weather of Bengaluru')
+  console.log(result?.finalOutput)
 }
 
-runAgentWithTool();
+runAgentWithTool()
